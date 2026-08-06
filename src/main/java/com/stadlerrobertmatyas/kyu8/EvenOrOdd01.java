e90129dc2f46a59190f07488bb811d32d0794f2e
Stadler Róbert Mátyás <stadlerrobertmatyas@protonmail.com>
2026-08-06 03:22:25 +0200

    8kyu - Even or Odd 1st solution

---
+++ src/main/java/com/stadlerrobertmatyas/kyu8/EvenOrOdd01.java
@@ -0,0 +1,12 @@
+package com.stadlerrobertmatyas.kyu8;
+
+public class EvenOrOdd01 {
+
+    // https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+    public static String evenOrOdd(int number) {
+        if (number % 2 == 0) {
+            return "Even";
+        }
+        return "Odd";
+    }
+}
---
+++ src/test/java/com/stadlerrobertmatyas/kyu8/EvenOrOddTest01.java
@@ -0,0 +1,34 @@
+package com.stadlerrobertmatyas.kyu8;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EvenOrOddTest01 {
+
+    @Test @DisplayName("Should return \"Odd\" for num = 1")
+    public void testPositiveOddNumbers() {
+        assertEquals("Odd", EvenOrOdd01.evenOrOdd(1));
+    }
+
+    @Test @DisplayName("Should return \"Even\" for num = 2")
+    public void testPositiveEvenNumbers() {
+        assertEquals("Even", EvenOrOdd01.evenOrOdd(2));
+    }
+
+    @Test @DisplayName("Should return \"Odd\" for num = -1")
+    public void testNegativeOddNumbers() {
+        assertEquals("Odd", EvenOrOdd01.evenOrOdd(-1));
+    }
+
+    @Test @DisplayName("Should return \"Even\" for num = -2")
+    public void testNegativeEvenNumbers() {
+        assertEquals("Even", EvenOrOdd01.evenOrOdd(-2));
+    }
+
+    @Test @DisplayName("Should return \"Even\" for num = 0")
+    public void testZero() {
+        assertEquals("Even", EvenOrOdd01.evenOrOdd(0));
+    }
+}
