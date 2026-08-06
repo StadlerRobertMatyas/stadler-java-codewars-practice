319366088dbb18c7de09c64c6a8d95c010694e3c
Stadler Róbert Mátyás <stadlerrobertmatyas@protonmail.com>
2026-08-06 03:11:30 +0200

    Initial commit

---
+++ src/main/java/com/stadlerrobertmatyas/Main.java
@@ -0,0 +1,8 @@
+package com.stadlerrobertmatyas;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.stadlerrobertmatyas;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
