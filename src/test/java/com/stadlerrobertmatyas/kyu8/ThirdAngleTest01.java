228930a77f9b5ec2afd3e15a2e671125ff004b32
Stadler Róbert Mátyás <stadlerrobertmatyas@protonmail.com>
2026-08-06 03:27:09 +0200

    8kyu - Third Angle of a Triangle 1st solution

---
+++ src/test/java/com/stadlerrobertmatyas/kyu8/ThirdAngleTest01.java
@@ -0,0 +1,15 @@
+package com.stadlerrobertmatyas.kyu8;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import org.junit.runners.JUnit4;
+
+public class ThirdAngleTest01 {
+    private Object ThirdAngle;
+
+    @Test
+    public void testOtherAngle() {
+        assertEquals(123, ThirdAngle01.otherAngle(45,12));
+        assertEquals(70, ThirdAngle01.otherAngle(50,60));
+    }
+}
