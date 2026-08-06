6788e9d8322476c9830f4aac4062758ffad8c200
Stadler Róbert Mátyás <stadlerrobertmatyas@protonmail.com>
2026-08-06 05:10:16 +0200

    8kyu - Multiply added link

--- src/main/java/com/stadlerrobertmatyas/kyu8/Multiply.java
+++ src/main/java/com/stadlerrobertmatyas/kyu8/Multiply.java
@@ -1,5 +1,6 @@
 package com.stadlerrobertmatyas.kyu8;
 
+// https://www.codewars.com/kata/50654ddff44f800200000004
 public class Multiply {
     public static Double multiply(Double a, Double b) {
         return a * b;
