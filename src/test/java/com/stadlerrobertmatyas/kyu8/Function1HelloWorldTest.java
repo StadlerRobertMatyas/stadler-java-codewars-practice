82e1e32601ab27c457ac13b3825ad17dd2a166d6
Stadler Róbert Mátyás <stadlerrobertmatyas@protonmail.com>
2026-08-06 04:11:06 +0200

    8kyu - Function 1 - hello worldű

---
+++ src/test/java/com/stadlerrobertmatyas/kyu8/Function1HelloWorldTest.java
@@ -0,0 +1,11 @@
+package com.stadlerrobertmatyas.kyu8;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class Function1HelloWorldTest {
+    @Test
+    public void testHelloWorld() throws Exception {
+        assertEquals("hello world!", Function1HelloWorld.greet());
+    }
+}
