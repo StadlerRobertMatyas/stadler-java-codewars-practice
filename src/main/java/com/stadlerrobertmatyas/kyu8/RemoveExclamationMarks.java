a72a0fa5be1659e4a211b9035c6848927df952a1
Stadler Róbert Mátyás <stadlerrobertmatyas@protonmail.com>
2026-08-06 03:38:53 +0200

    8kyu - Remove exclamation marks

---
+++ src/main/java/com/stadlerrobertmatyas/kyu8/RemoveExclamationMarks.java
@@ -0,0 +1,9 @@
+package com.stadlerrobertmatyas.kyu8;
+
+public class RemoveExclamationMarks {
+
+    // https://www.codewars.com/kata/57a0885cbb9944e24c00008e
+    static String removeExclamationMarks(String s) {
+        return s.replace("!", "");
+    }
+}
---
+++ src/test/java/com/stadlerrobertmatyas/kyu8/RemoveExclamationMarksTest.java
@@ -0,0 +1,12 @@
+package com.stadlerrobertmatyas.kyu8;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class RemoveExclamationMarksTest {
+
+    @Test
+    public void testSimpleString1() {
+        assertEquals("Hello World", RemoveExclamationMarks.removeExclamationMarks("Hello World!"));
+    }
+}
