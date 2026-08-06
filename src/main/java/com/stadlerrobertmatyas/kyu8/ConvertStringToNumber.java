d76d0d5c2f21e30fcf88f6e9983a123ba4b1199a
Stadler Róbert Mátyás <stadlerrobertmatyas@protonmail.com>
2026-08-06 04:02:38 +0200

    8kyu - Convert a String to a Number!

---
+++ src/main/java/com/stadlerrobertmatyas/kyu8/ConvertStringToNumber.java
@@ -0,0 +1,10 @@
+package com.stadlerrobertmatyas.kyu8;
+
+// https://www.codewars.com/kata/544675c6f971f7399a000e79
+public class ConvertStringToNumber {
+
+    public static int stringToNumber(String str) {
+        int number = Integer.parseInt(str);
+        return number;
+    }
+}
