db6214806d66b93ba276291a7e4c98adcf3fa46e
Stadler Róbert Mátyás <stadlerrobertmatyas@protonmail.com>
2026-08-06 03:54:38 +0200

    8kyu - Keep Hydrated!

---
+++ src/main/java/com/stadlerrobertmatyas/kyu8/KeepHydrated.java
@@ -0,0 +1,9 @@
+package com.stadlerrobertmatyas.kyu8;
+
+// https://www.codewars.com/kata/582cb0224e56e068d800003c
+public class KeepHydrated {
+
+    public static int liters(double time)  {
+        return (int) Math.floor(time * 0.5);
+    }
+}
