df070acd25f6d39eaf86904d734fc8da6f978aa6
Stadler Róbert Mátyás <stadlerrobertmatyas@protonmail.com>
2026-08-06 03:49:01 +0200

    8kyu - You Can't Code Under Pressure #1

---
+++ src/main/java/com/stadlerrobertmatyas/kyu8/YouCantCodeUnderPressure.java
@@ -0,0 +1,9 @@
+package com.stadlerrobertmatyas.kyu8;
+
+// https://www.codewars.com/kata/53ee5429ba190077850011d4
+public class YouCantCodeUnderPressure {
+
+    public static int doubleInteger(int i) {
+        return i * 2;
+    }
+}
---
+++ src/test/java/com/stadlerrobertmatyas/kyu8/YouCantCodeUnderPressureTest.java
@@ -0,0 +1,10 @@
+package com.stadlerrobertmatyas.kyu8;
+
+import org.junit.*;
+
+public class YouCantCodeUnderPressureTest {
+    @Test
+    public final void test_all() {
+        Assert.assertEquals( "Nope!" , 4, YouCantCodeUnderPressure.doubleInteger(2)); // Test cases here!
+    }
+}
