0a1504f9947669a2e55e33d282af71778c2bc6ee
Stadler Róbert Mátyás <stadlerrobertmatyas@protonmail.com>
2026-08-06 04:46:31 +0200

    7kyu - Growth of a Population 2nd solution

---
+++ src/main/java/com/stadlerrobertmatyas/kyu7/GrowthOfPopulation02.java
@@ -0,0 +1,20 @@
+package com.stadlerrobertmatyas.kyu7;
+
+// https://www.codewars.com/kata/563b662a59afc2b5120000c6
+public class GrowthOfPopulation02 {
+
+    public static int nbYear(int p0, double percent, int aug, int p) {
+        int year = 0;
+        double currentPopulation = p0;
+        double growthRate = percent / 100.0;
+
+        if (p0 >= p) {
+            return 0;
+        }
+        while (currentPopulation < p) {
+            currentPopulation = Math.floor(currentPopulation + currentPopulation * growthRate + aug);
+            year++;
+        }
+        return year;
+    }
+}
