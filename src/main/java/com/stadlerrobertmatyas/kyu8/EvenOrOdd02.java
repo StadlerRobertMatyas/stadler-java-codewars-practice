70539f03f670d2b7a31c6c3be88844170c348567
Stadler Róbert Mátyás <stadlerrobertmatyas@protonmail.com>
2026-08-06 03:22:32 +0200

    8kyu - Even or Odd 2nd solution

---
+++ src/main/java/com/stadlerrobertmatyas/kyu8/EvenOrOdd02.java
@@ -0,0 +1,9 @@
+package com.stadlerrobertmatyas.kyu8;
+
+public class EvenOrOdd02 {
+
+    // https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+    public static String evenOrOdd(int number) {
+        return (number % 2 == 0) ? "Even" : "Odd";
+    }
+}
