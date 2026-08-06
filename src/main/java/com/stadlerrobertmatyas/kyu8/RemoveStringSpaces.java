16667c6b52133c5db2073d2edda5c69d2d100fd3
Stadler Róbert Mátyás <stadlerrobertmatyas@protonmail.com>
2026-08-06 04:31:31 +0200

    8kyu - Remove String Spaces

---
+++ src/main/java/com/stadlerrobertmatyas/kyu8/RemoveStringSpaces.java
@@ -0,0 +1,9 @@
+package com.stadlerrobertmatyas.kyu8;
+
+// https://www.codewars.com/kata/57eae20f5500ad98e50002c5
+public class RemoveStringSpaces {
+
+    public static String noSpace(final String x) {
+        return x.replaceAll(" ",  "");
+    }
+}
