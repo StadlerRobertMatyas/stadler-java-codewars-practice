96b125dcfe72ed36fd2a79134f9310dd11f6b4c4
Stadler Róbert Mátyás <stadlerrobertmatyas@protonmail.com>
2026-08-09 23:32:49 +0200

    8kyu - Push a hash/an object into array added the link of the kata

--- src/main/java/com/stadlerrobertmatyas/kyu8/PushAnObjectIntoArray.java
+++ src/main/java/com/stadlerrobertmatyas/kyu8/PushAnObjectIntoArray.java
@@ -3,7 +3,9 @@ package com.stadlerrobertmatyas.kyu8;
 import java.util.List;
 import java.util.ArrayList;
 
+// https://www.codewars.com/kata/527b3cd0492b6b15250060af
 public class PushAnObjectIntoArray {
+
     public static List<String> push(){
         List<String> items = new ArrayList<>();
         items.add("an object");
