e12cfcc0bbef108c1b92c08e63a41d028ac0e87b
Stadler Róbert Mátyás <stadlerrobertmatyas@protonmail.com>
2026-08-09 23:31:21 +0200

    8kyu - Push a hash/an object into array

---
+++ src/test/java/com/stadlerrobertmatyas/kyu8/PushAnObjectIntoArrayTest.java
@@ -0,0 +1,15 @@
+package com.stadlerrobertmatyas.kyu8;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import java.util.List;
+
+public class PushAnObjectIntoArrayTest {
+
+    @Test
+    public void testSomething() {
+        List<String> items = PushAnObjectIntoArray.push();
+        assertEquals(1,items.size());
+        assertEquals("an object", items.get(0));
+    }
+}
