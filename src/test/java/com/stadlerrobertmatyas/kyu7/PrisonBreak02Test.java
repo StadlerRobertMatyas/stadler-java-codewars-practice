986d4e0acd0c99430f30b65fb341cd55d82d3d9e
Stadler Róbert Mátyás <stadlerrobertmatyas@protonmail.com>
2026-08-11 14:16:26 +0200

    7kyu - Prison Break updated tests to use PrisonBreak02

--- src/test/java/com/stadlerrobertmatyas/kyu7/PrisonBreak02Test.java
+++ src/test/java/com/stadlerrobertmatyas/kyu7/PrisonBreak02Test.java
@@ -6,16 +6,17 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PrisonBreak02Test {
+
     @Test
     @DisplayName("Fixed basic tests")
     void testBasicCases() {
-        assertEquals(4, PrisonBreak01.freedPrisoners(new Boolean[] { true, true, false, false, false, true, false }));
-        assertEquals(2, PrisonBreak01.freedPrisoners(new Boolean[] { true, false, false, false, false, false, false }));
-        assertEquals(2, PrisonBreak01.freedPrisoners(new Boolean[] { true, true, true, false, false, false }));
-        assertEquals(6, PrisonBreak01.freedPrisoners(new Boolean[] { true, false, true, false, true, false }));
-        assertEquals(1, PrisonBreak01.freedPrisoners(new Boolean[] { true, true, true }), "once the first prisoner is freed, all cells become locked");
-        assertEquals(0, PrisonBreak01.freedPrisoners(new Boolean[] { false, false, false }), "the first cell is locked, so no switches are possible");
-        assertEquals(0, PrisonBreak01.freedPrisoners(new Boolean[] { false, true, true, true }), "the first cell is locked, so no switches are possible");
+        assertEquals(4, PrisonBreak02.freedPrisoners(new Boolean[] { true, true, false, false, false, true, false }));
+        assertEquals(2, PrisonBreak02.freedPrisoners(new Boolean[] { true, false, false, false, false, false, false }));
+        assertEquals(2, PrisonBreak02.freedPrisoners(new Boolean[] { true, true, true, false, false, false }));
+        assertEquals(6, PrisonBreak02.freedPrisoners(new Boolean[] { true, false, true, false, true, false }));
+        assertEquals(1, PrisonBreak02.freedPrisoners(new Boolean[] { true, true, true }), "once the first prisoner is freed, all cells become locked");
+        assertEquals(0, PrisonBreak02.freedPrisoners(new Boolean[] { false, false, false }), "the first cell is locked, so no switches are possible");
+        assertEquals(0, PrisonBreak02.freedPrisoners(new Boolean[] { false, true, true, true }), "the first cell is locked, so no switches are possible");
     }
 }
 
