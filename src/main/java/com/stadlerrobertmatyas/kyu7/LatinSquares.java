2eabf1dfbd370616f545eec96e859d2b43b9e623
Stadler Róbert Mátyás <stadlerrobertmatyas@protonmail.com>
2026-08-11 12:09:36 +0200

    7kyu - Latin Squares

---
+++ src/main/java/com/stadlerrobertmatyas/kyu7/LatinSquares.java
@@ -0,0 +1,16 @@
+package com.stadlerrobertmatyas.kyu7;
+
+public class LatinSquares {
+
+    public static int[][] makeLatinSquare(int n) {
+        final int[][] latinSquare = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                latinSquare[i][j] = (i + j) % n + 1;
+            }
+        }
+
+        return latinSquare;
+    }
+}
